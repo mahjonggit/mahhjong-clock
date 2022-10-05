b380da1ccbe8e5ae5642aa738d04eb801e1d7ef6
mahjonggit <115097454+mahjonggit@users.noreply.github.com>
2022-10-06 00:13:47 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa'
 
 const config: UserConfig = {
 	plugins: [
-		sveltekit(),
-		VitePWA({
-			workbox: {
-                globPatterns: ['**/*.{js,css,html,svg,png,woff2}'],
-            },
-            registerType: "autoUpdate",
-            manifest: {
-                "background_color": "#ffffff",
-                "theme_color": "#7E1F86",
-                "name": "Mahjong",
-                "short_name": "Mahjong",
-                "start_url": "/",
-                "display": "standalone",
-                "icons": [
-                    {
-                        "src": "favicon.png",
-                        "sizes": "1000x1000",
-                        "type": "image/png",
-                        "purpose": "maskable any"
-                    }
-                ]
-            }
-		})
+		sveltekit()
 	]
 };
 
